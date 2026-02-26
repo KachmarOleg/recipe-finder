b42a8c1121c2f39472d1f8b3c3337bc10ab1ccc8
oleh.kachmar <oleh.kachmar@castfromclay.co.uk>
2026-02-26 21:32:51 +0100

    feat: add ingredient-tags from ingredient list

--- src/components/searchIngredients/SearchIngredients.jsx
+++ src/components/searchIngredients/SearchIngredients.jsx
@@ -1,8 +1,10 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useMemo, useRef } from "react";
 import { ingredients } from "../../data/ingredients";
 
 export default function SearchIngredients() {
   const [search, setSearch] = useState("");
+  const [selectedIngredients, setSelectedIngredients] = useState([]);
+  const ingredientList = useRef();
 
   const sortedIngredients = useMemo(() => {
     return [...ingredients].sort((a, b) => a.name.localeCompare(b.name));
@@ -16,6 +18,42 @@ export default function SearchIngredients() {
     setSearch(event.target.value);
   }
 
+  function handleSearchFocus() {
+    ingredientList.current.style.display = "flex";
+  }
+
+  function hideIngredientList() {
+    setTimeout(() => {
+      ingredientList.current.style.display = "none";
+    }, 100);
+  }
+
+  function handleSearchBlur() {
+    hideIngredientList();
+  }
+
+  function handleIngredientClick(event) {
+    console.log(event.target.value);
+    setSelectedIngredients([]);
+  }
+
+  function handleAddIngredient(title) {
+    const thebiggerID =
+      selectedIngredients.length > 0
+        ? selectedIngredients.slice(-1)[0]
+        : { id: 0 };
+
+    setSelectedIngredients([
+      ...selectedIngredients,
+      {
+        id: thebiggerID.id + 1,
+        name: title,
+      },
+    ]);
+
+    hideIngredientList();
+  }
+
   return (
     <div className="search-section">
       <input
@@ -24,13 +62,28 @@ export default function SearchIngredients() {
         placeholder="Type an ingredient (e.g. carrot, onion, potato...)"
         value={search}
         onChange={handleSearchChange}
+        onFocus={handleSearchFocus}
+        onBlur={handleSearchBlur}
       />
 
-      <ul className="ingredient-list">
+      <ul className="ingredient-list" ref={ingredientList}>
         {filteredIngredients.map((ingredient) => (
-          <li key={ingredient.id}>{ingredient.name}</li>
+          <li
+            key={ingredient.id}
+            onClick={(e) => handleAddIngredient(ingredient.name)}
+          >
+            {ingredient.name}
+          </li>
         ))}
       </ul>
+
+      <div className="tags">
+        {selectedIngredients.map((ingredient) => (
+          <div className="tag" key={ingredient.id}>
+            {ingredient.name} <span>×</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
