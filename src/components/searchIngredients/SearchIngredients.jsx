31595aa54f620e8834010f5e2cf55c5e1b1bccd9
oleh.kachmar <oleh.kachmar@castfromclay.co.uk>
2026-02-27 19:14:50 +0100

    feat: add close tags functionality

--- src/components/searchIngredients/SearchIngredients.jsx
+++ src/components/searchIngredients/SearchIngredients.jsx
@@ -3,12 +3,13 @@ import { ingredients } from "../../data/ingredients";
 
 export default function SearchIngredients() {
   const [search, setSearch] = useState("");
+  const [ingredientsList, setIngredientsList] = useState(ingredients);
   const [selectedIngredients, setSelectedIngredients] = useState([]);
-  const ingredientList = useRef();
+  const listUI = useRef();
 
   const sortedIngredients = useMemo(() => {
-    return [...ingredients].sort((a, b) => a.name.localeCompare(b.name));
-  }, [ingredients]);
+    return [...ingredientsList].sort((a, b) => a.name.localeCompare(b.name));
+  }, [ingredientsList]);
 
   const filteredIngredients = sortedIngredients.filter((ingredient) =>
     ingredient.name.toLowerCase().includes(search.toLowerCase()),
@@ -19,39 +20,51 @@ export default function SearchIngredients() {
   }
 
   function handleSearchFocus() {
-    ingredientList.current.style.display = "flex";
+    listUI.current.style.display = "flex";
   }
 
-  function hideIngredientList() {
+  function hideListUI() {
     setTimeout(() => {
-      ingredientList.current.style.display = "none";
+      listUI.current.style.display = "none";
     }, 100);
   }
 
   function handleSearchBlur() {
-    hideIngredientList();
+    hideListUI();
   }
 
-  function handleIngredientClick(event) {
-    console.log(event.target.value);
-    setSelectedIngredients([]);
-  }
-
-  function handleAddIngredient(title) {
-    const thebiggerID =
-      selectedIngredients.length > 0
-        ? selectedIngredients.slice(-1)[0]
-        : { id: 0 };
+  function handleAddIngredient(id, title) {
+    setIngredientsList(
+      ingredientsList.filter((ingr) => {
+        return ingr.id !== id;
+      }),
+    );
 
     setSelectedIngredients([
       ...selectedIngredients,
       {
-        id: thebiggerID.id + 1,
+        id: id,
         name: title,
       },
     ]);
 
-    hideIngredientList();
+    hideListUI();
+  }
+
+  function handleTagRemove(ingredientId, ingredientTitle) {
+    setSelectedIngredients(
+      selectedIngredients.filter((ingr) => {
+        return ingr.id !== ingredientId;
+      }),
+    );
+
+    setIngredientsList([
+      ...ingredientsList,
+      {
+        id: ingredientId,
+        name: ingredientTitle,
+      },
+    ]);
   }
 
   return (
@@ -66,11 +79,11 @@ export default function SearchIngredients() {
         onBlur={handleSearchBlur}
       />
 
-      <ul className="ingredient-list" ref={ingredientList}>
+      <ul className="ingredient-list" ref={listUI}>
         {filteredIngredients.map((ingredient) => (
           <li
             key={ingredient.id}
-            onClick={(e) => handleAddIngredient(ingredient.name)}
+            onClick={(e) => handleAddIngredient(ingredient.id, ingredient.name)}
           >
             {ingredient.name}
           </li>
@@ -80,7 +93,26 @@ export default function SearchIngredients() {
       <div className="tags">
         {selectedIngredients.map((ingredient) => (
           <div className="tag" key={ingredient.id}>
-            {ingredient.name} <span>×</span>
+            {ingredient.name}{" "}
+            <button
+              onClick={() => handleTagRemove(ingredient.id, ingredient.name)}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-x h-3.5 w-3.5"
+              >
+                <path d="M18 6 6 18"></path>
+                <path d="m6 6 12 12"></path>
+              </svg>
+            </button>
           </div>
         ))}
       </div>
