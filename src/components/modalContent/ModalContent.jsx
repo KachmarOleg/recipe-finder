90141dc9311ea2a343b346f62733a5817ca57b1a
oleh.kachmar <oleh.kachmar@castfromclay.co.uk>
2026-03-09 15:09:38 +0100

    refactor: extract CloseIcon component

--- src/components/modalContent/ModalContent.jsx
+++ src/components/modalContent/ModalContent.jsx
@@ -1,4 +1,5 @@
 import CheckIcon from "../../icons/checkIcon";
+import CloseIcon from "../../icons/CloseIcon";
 import InfoIcon from "../../icons/InfoIcon";
 import classes from "./ModalContent.module.scss";
 
@@ -32,21 +33,7 @@ export default function ModalCard({ recipe, setModal, selectedIngredients }) {
           setModal(false);
         }}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="lucide lucide-x h-3.5 w-3.5"
-        >
-          <path d="M18 6 6 18"></path>
-          <path d="m6 6 12 12"></path>
-        </svg>
+        <CloseIcon size={30} />
       </button>
 
       <figure>
--- src/components/tagsList/TagsList.jsx
+++ src/components/tagsList/TagsList.jsx
@@ -1,3 +1,4 @@
+import CloseIcon from "../../icons/CloseIcon";
 import classes from "./TagsList.module.scss";
 
 export default function TagsList({
@@ -27,21 +28,7 @@ export default function TagsList({
             className={classes.tagButton}
             onClick={() => handleTagRemove(ingredient.id, ingredient.name)}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="lucide lucide-x h-3.5 w-3.5"
-            >
-              <path d="M18 6 6 18"></path>
-              <path d="m6 6 12 12"></path>
-            </svg>
+            <CloseIcon />
           </button>
           {ingredient.name}{" "}
         </div>
